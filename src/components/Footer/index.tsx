6305aabcda126796341d640185e87f47ae1a0555
Stelios Tsinontas <41406137+stelisevil@users.noreply.github.com>
2025-08-17 20:43:24 +0100

    added icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,20 @@
+import facebook from "./facebook.png";
+import email from "./email.png";
+import phone from "./phone.png";
+
 export const Footer = () => (
   <footer className="p-1 text-xs font-bold text-white md:p-6 md:text-2xl bg-brand-dark">
     <div className="flex justify-between mx-auto max-w-7xl">
       <div>© 2025 by Steve McGarvie.</div>
-      <div className="flex flex-row">
-        <div>Some icons lol</div>
-        <div>07817 062951 (voicemail or text)</div>
+      <div className="flex flex-row gap-4">
+        <div className="flex flex-row items-center gap-3">
+          <img className="h-8" src={email} alt="email" />
+          <img className="h-8" src={facebook} alt="facebook logo" />
+          <img className="h-8" src={phone} alt="phone" />
+        </div>
+        <div>
+          07817 062951 <span className="font-normal">(voicemail or text)</span>
+        </div>
       </div>
     </div>
   </footer>
