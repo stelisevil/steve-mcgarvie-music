2779f1db9d0bd04dd70e39aec5d649d2c5743654
Stelios Tsinontas <41406137+stelisevil@users.noreply.github.com>
2025-11-08 16:13:34 +0000

    stupid build was stupid

--- src/pages/Bands/index.tsx
+++ src/pages/Bands/index.tsx
@@ -24,8 +24,8 @@ export const Bands = () => (
           src="https://www.youtube.com/embed/Go3oojnulkM?si=aWj357dkm-Opavya"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
         ></iframe>
       </div>
 
