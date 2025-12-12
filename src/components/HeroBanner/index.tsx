777854af9a3966d4eb79b95b06d41dc5fbce1930
Stelios Tsinontas <41406137+stelisevil@users.noreply.github.com>
2025-12-12 19:20:44 +0000

    Fixed HeroBanner not pulling images correctly

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -5,9 +5,9 @@ type HeroBannerProps = { children: React.ReactNode; imagePath: string };
 export const HeroBanner = ({ children, imagePath }: HeroBannerProps) => (
   <div
     className={classNames(
-      "h-[320px] md:h-[550px] bg-cover bg-center text-white text-4xl md:text-6xl flex justify-center items-end p-6 md:p-12 font-semibold",
-      `bg-[url(/hero-images/${imagePath})]`
+      "h-[320px] md:h-[550px] bg-cover bg-center text-white text-4xl md:text-6xl flex justify-center items-end p-6 md:p-12 font-semibold"
     )}
+    style={{ backgroundImage: `url(/hero-images/${imagePath})` }}
   >
     {children}
   </div>
